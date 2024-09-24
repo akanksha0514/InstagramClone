61cd4e19fe245ce88e20926bd20aee6f58d020a4
Akanksha Upadhyay <akankshaupadhyay@Akankshas-MacBook-Pro.local>
2024-09-24 12:42:45 +0100

    Initial commit

---
+++ InstagramClone/InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Akanksha Upadhyay on 24/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
